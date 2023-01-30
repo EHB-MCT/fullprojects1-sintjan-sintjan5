60dcecac1f45aa6314f0f76f7f7424add1517db0
GloryMenga <113978739+GloryMenga@users.noreply.github.com>
2023-01-30 16:06:47 +0100

    next try

--- Search.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("search-form");
-const input = document.getElementById("search-input");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  const query = input.value;
-  window.location.href = `/GloryMenga/Opvoedings.html${encodeURIComponent(query)}`;
-});
