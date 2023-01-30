9c0c2cac7b6e725fa97382dac966c32809ce3ec3
GloryMenga <113978739+GloryMenga@users.noreply.github.com>
2023-01-30 23:34:44 +0100

    ico

--- Search.js
+++ Search.js
@@ -2,13 +2,13 @@
 
 function search() {
 
-    var searchInput = document.getElementById("searchInput").value;
+    var searchInput = document.getElementById("searchInput").value.toLowerCase();
     
     if( searchInput === "opvoedingsproject"){
         window.open("/GloryMenga/Opvoedings.html");    
       }
 
     if( searchInput === "schoolvisie"){
-      window.open("/NelleFavoreel/schoolvisie.html");
+      window.open("/NelleFavoreel/schoolvisie.html");    
     }
   }
