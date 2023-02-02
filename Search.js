a156222f9ccaf05e861a6efd08b699f462ab9261
GloryMenga <113978739+GloryMenga@users.noreply.github.com>
2023-02-02 20:00:50 +0100

    mlj

--- Search.js
+++ Search.js
@@ -5,51 +5,51 @@ function search() {
     var searchInput = document.getElementById("searchInput").value.toLowerCase();
     
     if( searchInput === "home"){
-      window.open("/GloryMenga/home.html");    
+      window.open("GloryMenga/home.html");    
     }
 
     if( searchInput === "opvoedingsproject"){
-        window.open("/GloryMenga/Opvoedings.html");    
+        window.open("GloryMenga/Opvoedings.html");    
     }
 
     if( searchInput === "schoolvisie"){
-      window.open("/NelleFavoreel/schoolvisie.html");    
+      window.open("NelleFavoreel/schoolvisie.html");    
     }
 
     if( searchInput === "historiek"){
-      window.open("/NassimEjja/History.html");    
+      window.open("NassimEjja/History.html");    
     }
 
     if( searchInput === "jan"){
-      window.open("/NelleFavoreel/janBerchmans.html");    
+      window.open("NelleFavoreel/janBerchmans.html");    
     }
 
     if( searchInput === "motto"){
-      window.open("/NelleFavoreel/motto.html");    
+      window.open("NelleFavoreel/motto.html");    
     }
 
     if( searchInput === "jaar"){
-      window.open("/NelleFavoreel/jaarthema.html");    
+      window.open("NelleFavoreel/jaarthema.html");    
     }
 
     if( searchInput === "inschrijvingen"){
-      window.open("/BerkayUnutkan/wieiswie.html");    
+      window.open("BerkayUnutkan/wieiswie.html");    
     }
 
     if( searchInput === "studierichting"){
-      window.open("/NordineElOuafrasi/Studierichtingen/studieaanbod.html");    
+      window.open("NordineElOuafrasi/Studierichtingen/studieaanbod.html");    
     }
 
     if( searchInput === "wieiswie"){
-      window.open("/BerkayUnutkan/wieiswie.html");    
+      window.open("BerkayUnutkan/wieiswie.html");    
     }
 
     if( searchInput === "vacatures"){
-      window.open("/BerkayUnutkan/vacaturesberkay.html");    
+      window.open("BerkayUnutkan/vacaturesberkay.html");    
     }
 
     if( searchInput === "contact"){
-      window.open("/NelleFavoreel/contact.html");    
+      window.open("NelleFavoreel/contact.html");    
     }
 
   }
